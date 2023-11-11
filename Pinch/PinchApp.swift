387c8c59ef666ee6a85fd2bab781457525e83c80
Salman Hasan <s.hasan@unguarded.com>
2023-11-10 19:29:13 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Salman Hasan on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
