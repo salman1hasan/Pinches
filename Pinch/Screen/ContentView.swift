39cf18d8e52b5be4b9f40f0bd44b3daa4f2eabd3
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-11 12:03:52 -0500

    Add files via upload

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,10 +11,21 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    
+    func resetImageState(){
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     var body: some View {
         NavigationView{
-          ZStack{
+        ZStack{
+            Color.clear
+              
             Image("magazine-front-cover")
                 .resizable()
                 .aspectRatio(contentMode:.fit)
@@ -22,6 +33,7 @@ struct ContentView: View {
                 .padding()
                 .shadow(color:.black.opacity(0.2), radius:12, x:2, y:2)
                 .opacity(isAnimating ? 1:0)
+                .offset(x: imageOffset.width, y: imageOffset.height)
                 .scaleEffect(imageScale)
                 .onTapGesture(count:2, perform: {
                     if imageScale == 1 {
@@ -30,11 +42,24 @@ struct ContentView: View {
                         }
                     }else{
                         withAnimation(.spring()){
-                            imageScale = 1
+                           resetImageState()
                         }
                     }
                 })
-            }
+        .gesture(
+            DragGesture()
+            .onChanged { value in
+            withAnimation(.linear(duration: 1)){
+            imageOffset = value.translation
+                }
+              }
+            .onEnded { _ in
+                if imageScale <= 1 {
+                    resetImageState()
+                    }
+                }
+           )
+        }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear(perform: {
@@ -42,6 +67,12 @@ struct ContentView: View {
                      isAnimating = true
                 }
             })
+            .overlay(
+        InfoPanelView(scale: imageScale, offset: imageOffset)
+            .padding(.horizontal)
+            .padding(.top,30)
+        , alignment: .top
+            )
         }
         .navigationViewStyle(.stack)
         }
